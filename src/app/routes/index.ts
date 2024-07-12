261b77fd317179dc4a83cc3ee66391faf50e3406
habib-153 <h.r.sihab155@gmail.com>
2024-07-12 18:53:47 +0600

    feat: add order route in index.js of routes file

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { ProductRoutes } from '../modules/Product/product.route';
+import { OrderRoutes } from '../modules/Order/order.route';
 
 const router = Router();
 
@@ -8,6 +9,10 @@ const moduleRoutes = [
     path: '/products',
     route: ProductRoutes,
   },
+  {
+    path: '/order',
+    route: OrderRoutes
+  }
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
