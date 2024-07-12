c33b363f5b2f422bf4541b9e1f22538039f7045f
habib-153 <h.r.sihab155@gmail.com>
2024-07-12 18:03:10 +0600

    feat: Add CRUD functionality to Product module
    The code changes in this commit add CRUD (Create, Read, Update, Delete) functionality to the Product module. It includes the implementation of the `createProduct`, `getAllProduct`, `getSingleProduct`, `updateProduct`, and `deleteProduct` functions in the `product.controller.ts` file.
    
    The purpose of these changes is to enable the creation, retrieval, updating, and deletion of products in the application. Each function sends a response with the appropriate status code, success message, and data.
    
    This commit follows the established convention of using a prefix to indicate the type of change, in this case, feat for a new feature.

--- src/app/modules/Product/product.controller.ts
+++ src/app/modules/Product/product.controller.ts
@@ -2,19 +2,70 @@ import { Request, Response } from "express";
 import catchAsync from "../../utils/catchAsync";
 import { ProductService } from "./product.service";
 import sendResponse from "../../utils/sendResponse";
+import httpStatus from "http-status";
 
+const createProduct = catchAsync(async(req: Request, res: Response) => {
+    const result = await ProductService.createProductIntoDB(req.body)
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Product Created Successfully',
+        data: result
+    })
+})
 const getAllProduct = catchAsync(async(req: Request, res: Response) => {
     const query = req.query;
     const result = await ProductService.getAllProductsFromDB(query)
 
     sendResponse(res, {
-        statusCode: 200,
+        statusCode: httpStatus.OK,
         success: true,
-        message: 'All Products Retrived Successfully',
+        message: 'All Products Retrieved Successfully',
+        data: result
+    })
+})
+
+const getSingleProduct = catchAsync(async(req: Request, res: Response) =>{
+    const { id } = req.params
+    const result = await ProductService.getSingleProductFromDB(id)
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Product Retrieved Successfully',
+        data: result
+    })
+})
+
+const updateProduct = catchAsync(async(req: Request, res: Response) => {
+    const { id } = req.params
+    const result = await ProductService.updateProductInDB(id, req.body)
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Product Updated Successfully',
+        data: result
+    })
+})
+
+const deleteProduct = catchAsync(async(req: Request, res: Response) => {
+    const { id } = req.params
+    const result = await ProductService.deleteProdFromDB(id)
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Product Deleted Successfully',
         data: result
     })
 })
 
 export const ProductController = {
-    getAllProduct
+    getAllProduct,
+    getSingleProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct
 }
